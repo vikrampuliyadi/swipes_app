182b169872887f398f33480151812a86e9dfbca6
Brandon Lo <brandonlo11@ucla.edu>
2023-02-19 11:42:39 -0800

    Front end home page design and background image

--- swipes/src/pages/Home.js
+++ swipes/src/pages/Home.js
@@ -1,16 +1,19 @@
-function Home() { return (
-<div className="App">
-  <header className="App-header">
-    <img src="{logo}" className="App-logo" alt="logo" />
-    <p>Edit <code>src/App.js</code> and save to reload.</p>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
-    </a>
-  </header>
-</div>
-); } export default App;
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>SwipeX</h1>
+        <h2>Welcome to SwipeX, where you can buy and sell meal swipes for the UCLA dining halls!</h2>
+        <div className="App-buttons">
+          <button className="App-signup-button">Sign Up</button>
+          <button className="App-login-button">Log In</button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;

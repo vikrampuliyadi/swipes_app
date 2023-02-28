468a9eca62b61018847f4b16d3b2fb06c519f64c
tej-thambi <tejthambi@gmail.com>
2023-02-28 15:36:05 -0800

    Initial backend set up
    -added code to server.js
    -created .env file

---
+++ swipes/backend/server.js
@@ -0,0 +1,43 @@
+// const express = require('express');
+// const app = express();
+
+// app.get('/', function(req, res) {
+//     res.send('SwipeX Backend!');
+//   });
+  
+//   app.listen(3000, function() {
+//     console.log('Server started on port 3000');
+//   });
+  
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+const connection = mongoose.connection;
+mongoose.connect(uri, {useNewUrlParser: true})
+  .then(() => {
+    connection.once('open', () => {
+      console.log("MongoDB database connection established successfully");
+    })
+    //console.log("MongoDB database connection established successfully");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB database:", error);
+  });
+
+// connection.once('open', () => {
+//   console.log("MongoDB database connection established successfully");
+// })
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});

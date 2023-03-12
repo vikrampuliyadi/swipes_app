32cdb5362d1e25071676a5ab3fd771c0372a0a8c
brandonlo11 <brandonlo2003@yahoo.com>
2023-03-11 16:01:19 -0800

    Updated Login and Signup pages

--- swipes/src/pages/Login.js
+++ swipes/src/pages/Login.js
@@ -1,28 +1,63 @@
 import React, { useState } from "react";
 import "./Login.css";
-import "./SignUp.css";
 
 import { Link } from "react-router-dom";
+
+// function Login() {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   return (
+//     <div className="container">
+//       <header>
+//         <h1 className="head">Login</h1>
+//       </header>
+//       <body className="form">
+//         <p className="title container">Email</p>
+//         <input className="loginText1 container" />
+//         <p className="title container">Password</p>
+//         <input className="loginText2 container" />
+//         <Link to="/Main">
+//           {" "}
+//           <button className="loginButton container">Login</button>
+//         </Link>
+//       </body>
+//     </div>
+//   );
+// }
+
+// export default Login;
+
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  
   return (
-    <div className="container">
-      <header>
-        <h1 className="head">Login</h1>
-      </header>
-      <body className="form">
-        <p className="title container">Email</p>
-        <input className="loginText1 container" />
-        <p className="title container">Password</p>
-        <input className="loginText2 container" />
+    <div className="background">
+      <div className="shape"></div>
+      <div className="shape1"></div>
+      <form>
+        <h3>Login</h3>
+
+        <label htmlFor="username">Email</label>
+        <input type="text" placeholder="Email" id="username" />
+
+        <label htmlFor="password">Password</label>
+        <input type="password" placeholder="Password" id="password" />
         <Link to="/Main">
           {" "}
-          <button className="loginButton container">Login</button>
+          <button>Log In</button>
         </Link>
-      </body>
+        {/* <div className="social">
+          <div className="go">
+            <i className="fab fa-google"></i> Google
+          </div>
+          <div className="fb">
+            <i className="fab fa-facebook"></i> Facebook
+          </div>
+        </div> */}
+      </form>
     </div>
   );
 }
--- swipes/src/pages/SignUp.js
+++ swipes/src/pages/SignUp.js
@@ -31,23 +31,55 @@ function SignUpPage() {
   };
 
   return (
-    <div className="container">
-      <header>
-        <h1 className="head">Sign Up</h1>
-      </header>
-      <body className="form">
-        <p className="cont firstName">First Name</p>
-        <input className="textBox1" />
-        <p className="cont lastName">Last Name</p>
-        <input className="textBox2" />
-        <p className="cont email">Email</p>
-        <input className="textBox3" />
-        <p className="cont password">Password</p>
-        <input className="textBox4" />
+    // <div className="container">
+    //   <header>
+    //     <h1 className="head">Sign Up</h1>
+    //   </header>
+    //   <body className="form">
+    //     <p className="cont firstName">First Name</p>
+    //     <input className="textBox1" />
+    //     <p className="cont lastName">Last Name</p>
+    //     <input className="textBox2" />
+    //     <p className="cont email">Email</p>
+    //     <input className="textBox3" />
+    //     <p className="cont password">Password</p>
+    //     <input className="textBox4" />
+    //     <Link to="/Main">
+    //       <button className="button">Create Account</button>{" "}
+    //     </Link>
+    //   </body>
+    // </div>
+    <div className="background">
+      <div className="shape"></div>
+      <div className="shape1"></div>
+      <form>
+        <h3>Sign Up</h3>
+
+        <label htmlFor="firstname">First Name</label>
+        <input type="text" placeholder="First Name" id="firstname" />
+
+        <label htmlFor="lastname">Last Name</label>
+        <input type="text" placeholder="Last Name" id="lastname" />
+
+        <label htmlFor="username">Email</label>
+        <input type="text" placeholder="Email" id="username" />
+
+        <label htmlFor="password">Password</label>
+        <input type="password" placeholder="Password" id="password" />
+
         <Link to="/Main">
-          <button className="button">Create Account</button>{" "}
+          {" "}
+          <button>Create Account</button>
         </Link>
-      </body>
+        {/* <div className="social">
+          <div className="go">
+            <i className="fab fa-google"></i> Google
+          </div>
+          <div className="fb">
+            <i className="fab fa-facebook"></i> Facebook
+          </div>
+        </div> */}
+      </form>
     </div>
   );
 }

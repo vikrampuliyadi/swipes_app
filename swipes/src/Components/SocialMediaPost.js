cafdcf9fc652fa141ae88909da77ec3cf4c1707d
vikrampuliyadi <vikram.puliyadi@gmail.com>
2023-03-15 15:57:12 -0700

    put handleSubmit on accept button instead of form

--- swipes/src/Components/SocialMediaPost.js
+++ swipes/src/Components/SocialMediaPost.js
@@ -80,45 +80,45 @@ function SocialMediaPost({
   };
   return (
     //CORRECT CSS AND STYLING
-    <form onClick={handleSubmit}>
-      <div>
-        <div className="body">
-          <div className="cards">
-            <div className="card">
-              <div className="content">
-                <h2 className="post-element post-author">{email}</h2>
-                <p className="post-element1 post-contact">
-                  Contact: {contactInfo}
-                </p>
-                <p className="post-element1 post-timepstamp">{timestamp}</p>
+    <div>
+      <div className="body">
+        <div className="cards">
+          <div className="card">
+            <div className="content">
+              <h2 className="post-element post-author">{email}</h2>
+              <p className="post-element1 post-contact">
+                Contact: {contactInfo}
+              </p>
+              <p className="post-element1 post-timepstamp">{timestamp}</p>
+            </div>
+            <div className="image">
+              <div>
+                <h4 className="post-element2 post-content">{message}</h4>
               </div>
-              <div className="image">
-                <div>
-                  <h4 className="post-element2 post-content">{message}</h4>
-                </div>
+            </div>
+            <div className="content2">
+              <div class="grid">
+                <h4 className="post-element post-dining-hall">
+                  Dining Hall: {diningHall}
+                </h4>
+                <h4 className="post-element post-request-date">
+                  Buying swipe for: {date}
+                </h4>
+                <h4 className="post-element post-swipe-price">
+                  Price: ${price}
+                </h4>
+                <h4 className="post-element post-payment-type">
+                  Payment Type: {paymentType}
+                </h4>
               </div>
-              <div className="content2">
-                <div class="grid">
-                  <h4 className="post-element post-dining-hall">
-                    Dining Hall: {diningHall}
-                  </h4>
-                  <h4 className="post-element post-request-date">
-                    Buying swipe for: {date}
-                  </h4>
-                  <h4 className="post-element post-swipe-price">
-                    Price: ${price}
-                  </h4>
-                  <h4 className="post-element post-payment-type">
-                    Payment Type: {paymentType}
-                  </h4>
-                </div>
-                <button classname="accept-btn">Accept</button>
-              </div>
+              <button classname="accept-btn" onClick={handleSubmit}>
+                Accept
+              </button>
             </div>
           </div>
         </div>
       </div>
-    </form>
+    </div>
   );
 }
 
